34b6d13bc46c6d341bddd31505d7f4d140495ed2
林连钦 <642704882@qq.com>
2016-08-17 11:17:35 -0500

    add libcurl and a pushbutton
    build the libcurl under linux and use it in this project by get.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,7 +3,16 @@
 #include <qaction.h>
 #include <qdebug.h>
 #include <qkeysequence.h>
-//#include <cur
+//#include <curl/easy.h>
+#include <curl/curl.h>
+#include <curl/multi.h>
+#ifndef WIN32
+#  include <unistd.h>
+#endif
+#include <iostream>
+
+
+CURL *curl_handle;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,9 +20,13 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    QAction *actionTranslate = new QAction();
-    actionTranslate->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_P));
-    connect(actionTranslate,SIGNAL(triggered(bool)),this,SLOT(TranslateSlot()));
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl_handle = curl_easy_init();
+
+//    QAction *actionTranslate = new QAction();
+//    actionTranslate->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_P));
+//    connect(actionTranslate,SIGNAL(triggered(bool)),this,SLOT(TranslateSlot()));
+    connect(ui->pushButton,SIGNAL(clicked(bool)),this,SLOT(TranslateSlot()));
 }
 
 MainWindow::~MainWindow()
@@ -21,12 +34,25 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+size_t print_data(void*ptr,size_t size,size_t mem,void*stream)
+{
+    std::cout<<"ptr"<<(char*)ptr<<" size: "<<size<<" mem: "<<mem<<" stream: "<<(char*)stream<<endl;
+    return 0;
+}
+
 void MainWindow::TranslateSlot()
 {
+    int res = 0;
     QString doctype = "json";
-    QString url = "http://fanyi.youdao.com/openapi.do?keyfrom=Translate007&"
-                  "key=1061359031&type=data&doctype=%1&version=1.1&q=%2";
     QString str = ui->lineEdit->text();
-    url.arg(doctype).arg(str);
-    qDebug()<<url;
+    QString url = QString("http://fanyi.youdao.com/openapi.do?keyfrom=Translate007&"
+                  "key=1061359031&type=data&doctype=%1&version=1.1&q=%2").arg(doctype).arg(str);
+    curl_easy_setopt(curl_handle,CURLOPT_URL,url.toLocal8Bit().data());
+    curl_easy_setopt(curl_handle,CURLOPT_WRITEFUNCTION,print_data);
+    res = curl_easy_perform(curl_handle);
+    if (res != CURLE_OK) {
+        qDebug()<<url<<"is not performing successfully";
+    }else{
+        qDebug() << url << "is performing successfully";
+    }
 }
